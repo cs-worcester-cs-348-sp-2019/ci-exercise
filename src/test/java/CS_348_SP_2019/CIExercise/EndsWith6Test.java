51370e0248ace674a25349eb79a06bb4cd4f271a
Shruti Nagpal <snagpal@worcester.edu>
2019-04-17 11:12:56 -0400

    Changed Name6

--- src/test/java/CS_348_SP_2019/CIExercise/EndsWith6Test.java
+++ src/test/java/CS_348_SP_2019/CIExercise/EndsWith6Test.java
@@ -11,7 +11,8 @@ public class EndsWith6Test {
 	public void testToString() {
 		EndsWith6 six = new EndsWith6();
 		// add your name to the expected string, after the all the other names in the form John Smith\n
-		assertEquals("EndsWith6 [names=Chris Radkowski\n]", six.toString());
+		//assertEquals("EndsWith6 [names=Chris Radkowski\n]", six.toString());
+		assertEquals("EndsWith6 [names=Shruti Nagpal\n]", six.toString());
 	}
 
 }
