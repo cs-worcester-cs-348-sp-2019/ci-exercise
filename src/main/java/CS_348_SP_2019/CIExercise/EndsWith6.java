211731736d649e17d1f7a7bfedd8b244c8380663
Oresti Duro <oduro@worcester.edu>
2019-04-17 09:15:38 -0400

    new request

--- src/main/java/CS_348_SP_2019/CIExercise/EndsWith6.java
+++ src/main/java/CS_348_SP_2019/CIExercise/EndsWith6.java
@@ -9,6 +9,7 @@ public class EndsWith6 {
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
 		names += "Oresti Duro Chris Radkowski\n";
+	}
 
 
 	@Override
