8224e14c4ae5a1857246e789d0af797afee9b126
Kyle LaPointe <git@gitlab.com:klapointe2>
2019-04-18 09:02:53 -0400

    add klapointe2

--- src/main/java/CS_348_SP_2019/CIExercise/EndsWith4.java
+++ src/main/java/CS_348_SP_2019/CIExercise/EndsWith4.java
@@ -6,9 +6,9 @@ public class EndsWith4 {
 
 	public EndsWith4()  {
 		names = "";
-		names += "Kyle LaPointe\n";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
+		names += "Kyle LaPointe\n";
 		
 	}
 
